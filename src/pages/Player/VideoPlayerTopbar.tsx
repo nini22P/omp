5016f1d42d674c404c058bfaf8434a39707a12ac
22 <60903333+nini22P@users.noreply.github.com>
2024-07-03 17:46:27 +0800

    style: update video topbar style

--- src/pages/Player/VideoPlayerTopbar.tsx
+++ src/pages/Player/VideoPlayerTopbar.tsx
@@ -27,30 +27,38 @@ const VideoPlayerTopbar = () => {
         top: 0,
         left: 0,
         width: '100%',
-        background: `${theme.palette.background.paper}99`,
         height: 'var(--titlebar-height)',
         transform: videoViewIsShow && controlIsShow ? 'none' : 'translateY(calc(-1 * var(--titlebar-height)))',
         transition: 'all 0.2s ease-out',
-        backdropFilter: 'blur(16px)',
       }}
     >
-      <IconButton
-        aria-label="close"
-        onClick={() => {
-          updateVideoViewIsShow(false)
-          updateControlIsShow(true)
+      <div
+        style={{
+          width: 'fit-content',
+          height: '100%',
+          display: 'flex',
+          background: `${theme.palette.background.paper}99`,
+          backdropFilter: 'blur(16px)',
         }}
         className='app-region-no-drag'
-        sx={{
-          height: 'var(--titlebar-height)',
-          borderRadius: '0.2rem',
-          '.MuiTouchRipple-ripple .MuiTouchRipple-child': {
-            borderRadius: '0.2rem',
-          },
-        }}
       >
-        <KeyboardArrowDownRoundedIcon />
-      </IconButton>
+        <IconButton
+          aria-label="close"
+          onClick={() => {
+            updateVideoViewIsShow(false)
+            updateControlIsShow(true)
+          }}
+          sx={{
+            height: '100%',
+            borderRadius: '0.25rem',
+            '.MuiTouchRipple-ripple .MuiTouchRipple-child': {
+              borderRadius: '0.25rem',
+            },
+          }}
+        >
+          <KeyboardArrowDownRoundedIcon />
+        </IconButton>
+      </div>
     </Box>
   )
 }
